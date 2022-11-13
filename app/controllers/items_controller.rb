26e2575042a70c8b0c010799c3b0d32d107ecbd1
supermasao <m.a.s.a.f.u.m.i.112@gmail.com>
2022-11-13 20:47:25 +0900

    アクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show,:update]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show,]
 
    def index
      @items = Item.order("created_at DESC")
@@ -19,20 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-     @item = Item.find(params[:id])
-   end
+  end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item.update(item_params)
+    if @item.save
       redirect_to root_path
     else
-      render :edit
+      render :"items/edit"
     end
   end
 
@@ -44,4 +43,18 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :product_name, :description, :category_id, :state_id, :payer_id, :prefecture_id,
                                  :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if not user_signed_in?
+      redirect_to action: :index
+    elsif current_user.id != @item.user_id 
+      redirect_to action: :index
+    end
+  end
+
+
 end
